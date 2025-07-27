b03d0e6cf24cdb23db0545828a4b5b89c8f945e8
mudsahni <muditsahni@msn.com>
2025-07-27 15:18:45 +0530

    updates

--- src/main/kotlin/service/RecordService.kt
+++ src/main/kotlin/service/RecordService.kt
@@ -2,14 +2,14 @@ package com.muditsahni.service.v1
 
 import com.muditsahni.service.command.UploadRequestCommand
 import com.muditsahni.repository.RecordRepository
-import com.muditsahni.service.AzureBlobStorageService
+//import com.muditsahni.service.AzureBlobStorageService
 import com.muditsahni.service.command.UploadRequestResult
 import java.util.UUID
 
 interface RecordService {
 
     val recordRepository: RecordRepository
-    val storageService: AzureBlobStorageService
+//    val storageService: AzureBlobStorageService
 
     suspend fun requestUpload(
         tenantId: String,
