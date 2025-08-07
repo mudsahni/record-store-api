e79231738beeea60c4b180ec009c5f8d93af5ccf
mudsahni <muditsahni@msn.com>
2025-08-07 17:21:22 +0530

    updates

--- src/main/kotlin/config/TenantContext.kt
+++ src/main/kotlin/config/TenantContext.kt
@@ -10,30 +10,32 @@ import kotlin.text.set
 
 @Component
 class TenantContext {
+    object TenantContextKey {
+        private val threadLocal = ThreadLocal<Tenant?>()
+
+        fun get(): Tenant? = threadLocal.get()
+        fun set(tenant: Tenant?) = threadLocal.set(tenant)
+    }
+
+    class TenantContextElement(private val tenant: Tenant) : ThreadContextElement<Tenant?> {
+        companion object Key : CoroutineContext.Key<TenantContextElement>
+
+        override val key: CoroutineContext.Key<TenantContextElement> = Key
+
+        override fun updateThreadContext(context: CoroutineContext): Tenant? {
+            val previousTenant = TenantContextKey.get()
+            TenantContextKey.set(tenant)
+            return previousTenant
+        }
+
+        override fun restoreThreadContext(context: CoroutineContext, oldState: Tenant?) {
+            TenantContextKey.set(oldState)
+        }
+    }
+
     companion object {
         val logger = mu.KotlinLogging.logger {}
 
-        object TenantContextKey {
-            private val threadLocal = ThreadLocal<Tenant?>()
-
-            fun get(): Tenant? = threadLocal.get()
-            fun set(tenant: Tenant?) = threadLocal.set(tenant)
-        }
-        class TenantContextElement(private val tenant: Tenant) : ThreadContextElement<Tenant?> {
-            companion object Key : CoroutineContext.Key<TenantContextElement>
-
-            override val key: CoroutineContext.Key<TenantContextElement> = Key
-
-            override fun updateThreadContext(context: CoroutineContext): Tenant? {
-                val previousTenant = TenantContextKey.get()
-                TenantContextKey.set(tenant)
-                return previousTenant
-            }
-
-            override fun restoreThreadContext(context: CoroutineContext, oldState: Tenant?) {
-                TenantContextKey.set(oldState)
-            }
-        }
 
         private val tenantContextKey = TenantContextKey
 
